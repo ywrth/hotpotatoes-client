097b290acdb2adafdeb4fcda5b969c8f4957d044
ywrth <zuatworknow@gmail.com>
2023-08-14 09:04:44 +0200

    component import

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,11 +1,14 @@
-export const MainView = () => {
-    return (
-      <div>
+export const MainView = () => { // exposing the main-view component - import components to the other files
+    return ( // only one root element, hence the big div
+    <React.Fragment> 
+      <div> 
         <div></div>
         <div></div>
         <div></div>
         <div></div>
         <div></div>
       </div>
+      <button>TEST</button>
+      </React.Fragment> // or I can use <> instead of react.fragment
     );
   };
--- src/index.jsx
+++ src/index.jsx
@@ -5,17 +5,13 @@ import { MainView } from './components/main-view/main-view';
 import "./index.scss";
 
 // Main component (will eventually use all the others)
-const MyFlixApplication = () => {
-  return (
-    <div className="my-flix">
-      <div>Good morning</div>
-    </div>
-  );
-};
+const App = () => {
+  return <MainView />;
+ };
 
 // Finds the root of your app
 const container = document.querySelector("#root");
 const root = createRoot(container);
 
 // Tells React to render your app in the root DOM element
-root.render(<MyFlixApplication />);
+root.render(<App />);
