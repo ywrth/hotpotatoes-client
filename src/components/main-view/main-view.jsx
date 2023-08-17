6863fa92135c7b0d8ff79750fc0a9c7b6eb328d6
ywrth <zuatworknow@gmail.com>
2023-08-17 12:19:56 +0200

    updates

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -33,7 +33,7 @@ export const MainView = () => {
 
   // Handle user login
   if (!user) {
-    return <LoginView />;
+    return <LoginView onLoggedIn={(user) => setUser(user)} />;
   }
 
   if (selectedMovie) {
