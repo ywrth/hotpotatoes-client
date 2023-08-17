6af456ce6df43a723cebc60025b05d618aaa8980
ywrth <zuatworknow@gmail.com>
2023-08-17 11:51:51 +0200

    login view + main view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types"; // Import PropTypes
 import { MovieCard } from "../movie-card/movie-card"; // Importing the MovieCard component
 import { MovieView } from "../movie-view/movie-view"; // Importing the MovieView component
