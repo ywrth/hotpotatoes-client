b9a65bb89d378eb687331b873ba3319d5f9ec9f3
ywrth <zuatworknow@gmail.com>
2023-08-17 10:26:33 +0200

    import prop-types

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import PropTypes from "prop-types"; // Import PropTypes
 import { MovieCard } from "../movie-card/movie-card"; // Importing the MovieCard component
 import { MovieView } from "../movie-view/movie-view"; // Importing the MovieView component
 
