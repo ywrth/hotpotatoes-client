d05cbf59dae6b9f25dbd2f0cc463defacba7fdce
ywrth <zuatworknow@gmail.com>
2023-09-05 09:35:01 +0200

    import link/movie card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 import "./movie-card.scss";
 
